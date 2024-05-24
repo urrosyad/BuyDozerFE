a79ff76f437d4f6424dd78abc001164b947b1342
Ulur Rosyad <ulurrosyad92033@gmail.com>
2024-05-24 14:32:37 +0700

    Adding Dashboard Report Page and cleaning the code

--- BuydozerAdmin/src/main.jsx
+++ BuydozerAdmin/src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 // import "./index.css"
 
+
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 60000, gcTime: 10 * (60 * 1000)}}
 })
