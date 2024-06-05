928e82ead5bdc6e7297ca06b0f2549a800fdd20b
Roellyx <ulurrosyad92033@gmail.com>
2024-06-05 14:25:22 +0700

    Configure it so that the deployment is successful

--- BuydozerAdmin/vite.config.js
+++ BuydozerAdmin/vite.config.js
@@ -1,28 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 2005
+    port: 2005,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    chunkSizeWarningLimit: 500, // Jika Anda ingin mengatasi peringatan chunk size
   },
   resolve: {
     alias: {
-      '@assets': '/src/assets',
-      '@src': '/src',
-      '@components': '/src/Components',
-      '@hooks': '/src/Hooks',
-      '@context': '/src/Context',
-      '@pages': '/src/Pages',
-      '@routers': '/src/Routers',
-      '@utils': '/src/Utils',
-      '@layouts': '/src/Layouts',
-      '@auth': '/src/auth',
-      '@schemas': '/src/Schemas',
-      '@themes': '/src/Themes',
-      '@service': '/src/Service',
-      '@api': '/src/api',
+      "@assets": "/src/assets",
+      "@src": "/src",
+      "@components": "/src/Components",
+      "@hooks": "/src/Hooks",
+      "@context": "/src/Context",
+      "@pages": "/src/Pages",
+      "@routers": "/src/Routers",
+      "@utils": "/src/Utils",
+      "@layouts": "/src/Layouts",
+      "@auth": "/src/auth",
+      "@schemas": "/src/Schemas",
+      "@themes": "/src/Themes",
+      "@service": "/src/Service",
+      "@api": "/src/api",
     },
   },
-})
+});
